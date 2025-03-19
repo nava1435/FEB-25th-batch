f4c470ae6ae139989e3d9d9936f8956e9fc48126
nava1435 <navaneeth.k1273@gmail.com>
2025-03-19 15:13:19 +0530

    filter,sort,pegination and delete operation

--- src/app/vehicle.service.ts
+++ src/app/vehicle.service.ts
@@ -16,5 +16,19 @@ export class VehicleService {
     return this._httpClient.get("https://api.open-meteo.com/v1/forecast?latitude=17.385044&longitude=78.486671&current_weather=true");
   }
  
+  // getFilterVehicles(term:any):Observable<any>{
+  //   return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?filter="+term);
+  // }
+  getSortedVehicles(column:any,order:any):Observable<any>{
+    return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?sortBy="+column+"&order="+order);
+  }
+
+  deleteVehicles(id:any):Observable<any>{
+    return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?sortBy/"+id);
+  }
+
+  getPaginatedVehicles(limit:any,page:any):Observable<any>{
+    return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?limit="+limit+"&page="+page);
+  }
 }
 
--- src/app/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle.component.ts
@@ -4,21 +4,93 @@ import { VehicleService } from '../vehicle.service';
 @Component({
   selector: 'app-vehicle',
   templateUrl: './vehicle.component.html',
-  styleUrls: ['./vehicle.component.css']
+  styleUrls: ['./vehicle.component.css'],
 })
 export class VehicleComponent {
-  vehicles:any;
-
-  constructor(private _vehicleService:VehicleService){
+  term: string = '';
+  vehicles: any = [];
+  constructor(private _vehicleService: VehicleService) {
+    this.loadVehicles();
     _vehicleService.getVehicles().subscribe(
-      (data:any)=>{ 
+      (data: any) => {
         console.log(data);
-        this.vehicles=data;
-       }, (err:any)=>{
-          alert("Internal server Error");
-        }
-      
-    ) 
+        this.vehicles = data;
+      },
+      (err: any) => {
+        alert('Internal server Error');
+      }
+    );
   }
+// ========Filter()============================
+  // filter() {
+  //   this._vehicleService.getFilterVehicles(this.term).subscribe(
+  //     (data: any) => {
+  //       console.log(data);
+  //       this.vehicles = data;
+  //     },
+  //     (err: any) => {
+  //       alert('Internal server Error');
+  //     }
+  //   );
+  // }
+// ==========Sorting()==================================
+  column:string='';
+  order:string='';
+sort(){
+  this._vehicleService.getSortedVehicles(this.column,this.order).subscribe(
+    (data:any)=>{
+      console.log(data);
+      this.vehicles=data;
+
+    },(err:any)=>{
+      alert("Internal Ser er Error");
+    }
+  )
+}
+
+//==============================================================================
+loadVehicles(){
+this._vehicleService.getVehicles().subscribe(
+  (data:any)=>{
+    console.log(this.vehicles);
+
+  },(err:any)=>{
+    alert("Internal server Error")
+  }
+)
 
 }
+delete(id:any){
+  if(confirm("Are you sure to delete")==true){
+  this._vehicleService.deleteVehicles(id).subscribe(
+  // this._vehicleService.deleteVehicle(id).subscribe(
+    (data:any)=>{
+      alert("Record Deleted Succesfully")
+    },(err:any)=>{
+      alert("Internal Server Error");
+    }
+  )
+
+}else{
+  alert("you have cancelled")
+}
+
+}
+limit:string='';
+page:string='';
+pagination(){
+  this._vehicleService.getPaginatedVehicles(this.limit,this.page).subscribe(
+    (data:any)=>{
+      console.log(data);
+      this.vehicles=data;
+
+    },(err:any)=>{
+      alert("Internal Server Error")
+    }
+  )
+}
+
+}
+
+
+
